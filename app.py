1405e88e1b5bacef4e1565fcf1b76a4308eba3d1
Mobarak Hosen Shakil <20867846+imShakil@users.noreply.github.com>
2022-04-23 10:51:44 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import shutil
 from plate_detector import plate_finder
 from flask import Flask, render_template, request
 
+GOV_DATA = 'database/data.txt'
 UPLOAD_FOLDER = '/static/uploads/'
 # clean the upload folder
 shutil.rmtree(os.getcwd() + UPLOAD_FOLDER)
@@ -13,7 +14,21 @@ os.makedirs(os.getcwd() + UPLOAD_FOLDER)
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
+raw_data = []
 
+with open(GOV_DATA, encoding='utf-8') as file:
+    raw_data = file.readlines()
+
+def find_data(text):
+    print(text)
+    text = text.replace('\n', ' ')
+    print(text)
+    for data in raw_data:
+        data = data.strip()
+        print(data)
+        if data == text:
+            return True
+    return False
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -42,8 +57,15 @@ def upload_page():
             # call the license plate detection function
             extracted_text = plate_finder(file)
             plate_name = 'plate.png'
+
+            valid_msg = "This is an invalid license number."
+            if find_data(extracted_text):
+                print("found")
+                valid_msg = "This is an authorized license number."
+
             return render_template('upload.html', msg='Successfully processed', extracted_text=extracted_text,
-                                   img_src=UPLOAD_FOLDER + file.filename, img_src1=UPLOAD_FOLDER + plate_name)
+                                   img_src=UPLOAD_FOLDER + file.filename, img_src1=UPLOAD_FOLDER + plate_name,
+                                   valid_msg=valid_msg)
     elif request.method == 'GET':
         return render_template('upload.html')
 
